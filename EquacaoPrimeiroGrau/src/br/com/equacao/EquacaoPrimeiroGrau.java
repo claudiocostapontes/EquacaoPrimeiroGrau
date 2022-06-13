0c26e7737b53b7e558803031d1d477e83bba26ef
Claudionor Pontes <claudionor.pontes@outlook.com>
2022-06-13 12:41:23 -0300

    Equação de Primeiro Grau
    Fórmula para resolver equações de primeiro grau.

---
+++ EquacaoPrimeiroGrau/src/br/com/equacao/EquacaoPrimeiroGrau.java
@@ -0,0 +1,11 @@
+package br.com.equacao;
+
+public interface EquacaoPrimeiroGrau<x> {
+
+// Métodos	
+	
+	public double String valor a (double valor);
+	public double String valor b (double valor);
+	
+	
+}
---
+++ EquacaoPrimeiroGrau/src/br/com/equacao/Principal.java
@@ -0,0 +1,18 @@
+package br.com.equacao;
+
+//Importar a classe JOptionPane
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+public static void main(String[] args) {
+
+// Instancia objeto da classe valor
+		
+		
+		
+		
+	}
+
+}
